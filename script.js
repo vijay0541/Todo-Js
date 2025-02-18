9e6985bfd3c6140e38b73e2c48709082b3fd1f35
vijay0541 <chaurasiyavijay29@gmail.com>
2025-02-18 12:13:31 +0530

    localStarage is used hear and re-rendering the each item when we update the items array

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 const inputText = document.getElementById("todo-input");
 const taskList = document.getElementById("task-to-add");
-const items = []; 
+let items = []; 
+
+// Load tasks from localStorage when the page loads or when when we open the origin in new tab
+function loadTasks() {
+    // Check if there are saved tasks in localStorage
+    const storedItems = localStorage.getItem('tasks');
+    if (storedItems) {
+        // Parse the stored tasks and assign them to the 'items' array to get actual object
+        items = JSON.parse(storedItems);
+        // Update the task list display from local storage
+        updateTaskList();
+    }
+}
+
+// Save tasks to localStorage
+function saveTasks() {
+    // Convert the 'items' array into a string and store it in localStorage
+    localStorage.setItem('tasks', JSON.stringify(items));
+}
 
 function addTask() {
     const val = inputText.value;
     if (val !== "") {
-        let newLi = document.createElement("li");
-        newLi.textContent = val;
         inputText.value = ""; 
-
         const existingItem = items.find(item => item.name === val);
-
         if (existingItem) {
             existingItem.count++;
         } else {
             items.push({ name: val, count: 1 });
         }
-
+        saveTasks();
         updateTaskList();
     }
 }
 
-function processItem(removedTask) {
+function deleteItem(removedTask) {
+
     const existingItem = items.find(item => item.name === removedTask);
 
     if (existingItem) {
@@ -33,12 +48,13 @@ function processItem(removedTask) {
                 items.splice(index, 1);
             }
         }
+        saveTasks(); 
     }
     updateTaskList();
 }
 
 function updateTaskList() {
-    taskList.innerHTML = ''; 
+    taskList.innerHTML = ''; // for re-rendering the task when update the items array
     items.forEach(item => {
         let newLi = document.createElement("li");
         newLi.textContent = `${item.name} - ${item.count}`;
@@ -47,7 +63,7 @@ function updateTaskList() {
         deleteBtn.textContent = "✅";
 
         deleteBtn.onclick = function () {
-            processItem(item.name);
+            deleteItem(item.name);
         };
 
         newLi.appendChild(deleteBtn);
@@ -55,3 +71,5 @@ function updateTaskList() {
     });
 }
 
+// Call loadTasks to populate the task list when the page loads
+window.onload = loadTasks;  // When the window is loaded, load tasks from localStorage
